de44d6be2fb30330a8c0d0ab52e5ef2452ed045f
SlowFlo <fbautry@gmail.com>
2023-11-14 20:09:43 +0100

    feature: add Status choices

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,6 +3,17 @@ from django.utils import timezone
 
 
 class Post(models.Model):
+
+    class Status(models.TextChoices):
+        # embeded class : allow to easily reference choice labels, values, or names from anywhere in the code
+        # ex: Post.Status.DRAFT
+        DRAFT = "DF", "Draft"
+        PUBLISHED = "PB", "Published"
+        # Post.Status.choices -> [('DF', 'Draft'), ('PB', 'Published')]
+        # Post.Status.labels  -> ['Draft', 'Published']
+        # Post.Status.values  -> ['DF', 'PB']
+        # Post.Status.names   -> ['DRAFT', 'PUBLISHED']
+
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
@@ -11,6 +22,9 @@ class Post(models.Model):
     # publish = models.DateTimeField(db_default=Now()) Same but with database-computed default
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=2,
+                              choices=Status,
+                              default=Status.DRAFT)
 
     class Meta:
         ordering = ["-publish"]
