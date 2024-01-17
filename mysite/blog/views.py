f3aec2dda0af06cb9ddfa07d3a7b37a4d1c57354
slowflo <fbautry@gmail.com>
2024-01-17 12:14:24 +0100

    feat: add similar posts section in blog post detail view
    In this commit, we have added code to display similar blog posts based on tags in the detailed view of each blog post. The new section lists up to four posts that share the most tags with the post currently being viewed, thereby enhancing user engagement by suggesting other relevant content.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,6 @@
 from django.core.mail import send_mail
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
@@ -56,14 +57,26 @@ def post_detail(request, year, month, day, post):
 
     # List of active comments for this post
     comments = post.comments.filter(active=True)
-
     # Form for users to comment
     form = CommentForm()
+
+    # List of similar posts
+    # flat=True -> [1, 2, 3, ...] instead of [(1,), (2,), (3,) ...]
+    post_tags_ids = post.tags.values_list("id", flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count("tags")).order_by(
+        "-same_tags", "-publish"
+    )[:4]
 
     return render(
         request,
         "blog/post/detail.html",
-        {"post": post, "comments": comments, "form": form},
+        {
+            "post": post,
+            "comments": comments,
+            "form": form,
+            "similar_posts": similar_posts,
+        },
     )
 
 
