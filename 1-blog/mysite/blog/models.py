60616e954a788bdc76ad705e4cbfb6ed1b3e83fa
SlowFlo <fbautry@gmail.com>
2026-01-27 00:41:31 +0100

    feat: add Post model with fields for blog posts
    Implemented the `Post` model in `blog/models.py` with fields for `title`, `slug`, `body`, `publish`, `created`, and `updated`. Added a `__str__` method for string representation.

--- 1-blog/mysite/blog/models.py
+++ 1-blog/mysite/blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    # if we want to use database-computed default value, we can do :
+    # from django.db.models.functions import Now
+    # publish = models.DateTimeField(db_default=Now())
+
+    def __str__(self):
+        return self.title
