061de89c46d36e4855724effc64568c6ebe77059
SlowFlo <fbautry@gmail.com>
2026-02-04 02:11:22 +0100

    refactor: simplify post_detail view with get_object_or_404
    Replaced custom exception handling for missing posts with `get_object_or_404` to streamline the retrieval of published posts.

--- 1-blog/mysite/blog/views.py
+++ 1-blog/mysite/blog/views.py
@@ -1,5 +1,4 @@
-from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from blog.models import Post
 
@@ -10,8 +9,9 @@ def post_list(request):
 
 
 def post_detail(request, id):
-    try:
-        post = Post.published.get(id=id)
-    except Post.DoesNotExist:
-        raise Http404("No Post found.")
+    # try:
+    #     post = Post.published.get(id=id)
+    # except Post.DoesNotExist:
+    #     raise Http404("No Post found.")
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
     return render(request, "blog/post/detail.html", {"post": post})
