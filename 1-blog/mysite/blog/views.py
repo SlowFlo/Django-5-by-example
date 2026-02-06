265b3ce6ae0e43dc3aa75179ea4023bc5ab5a7e9
SlowFlo <fbautry@gmail.com>
2026-02-06 20:05:47 +0100

    chore: update Django to 5.2 and reformat views for better readability

--- 1-blog/mysite/blog/views.py
+++ 1-blog/mysite/blog/views.py
@@ -5,7 +5,11 @@ from blog.models import Post
 
 def post_list(request):
     posts = Post.published.all()
-    return render(request, "blog/post/list.html", {"posts": posts})
+    return render(
+        request,
+        "blog/post/list.html",
+        {"posts": posts},
+    )
 
 
 def post_detail(request, id):
@@ -13,5 +17,13 @@ def post_detail(request, id):
     #     post = Post.published.get(id=id)
     # except Post.DoesNotExist:
     #     raise Http404("No Post found.")
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
-    return render(request, "blog/post/detail.html", {"post": post})
+    post = get_object_or_404(
+        Post,
+        id=id,
+        status=Post.Status.PUBLISHED,
+    )
+    return render(
+        request,
+        "blog/post/detail.html",
+        {"post": post},
+    )
