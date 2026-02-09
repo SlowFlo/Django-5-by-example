f05c2904e78ef354283815939d565a416a8ec0c0
SlowFlo <fbautry@gmail.com>
2026-02-09 02:52:59 +0100

    feat: add comment functionality to blog posts
    Implemented a `Comment` model associated with posts, including a migration file. Added support for submitting and displaying comments using a comment form and the `post_comment` view. Updated the post detail template to show existing comments and include the form for new submissions. Enhanced admin interface with comment management features.

--- 1-blog/mysite/blog/forms.py
+++ 1-blog/mysite/blog/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
+from blog.models import Comment
+
 
 class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=25)
@@ -9,3 +11,26 @@ class EmailPostForm(forms.Form):
         required=False,
         widget=forms.Textarea,
     )
+
+
+# New in Django 5.2, styling your forms is easier than ever with the simplified override of
+# a form’s BoundField. With CommentBoundField, comment form fields are now wrapped
+# in a div with the comment class.
+class CommentBoundField(forms.BoundField):
+
+    comment_class = "comment"
+
+    def css_classes(self, extra_classes=None):
+        result = super().css_classes(extra_classes)
+        if self.comment_class not in result:
+            # add the custom class
+            result += f" {self.comment_class}"
+        return result.strip()
+
+
+class CommentForm(forms.ModelForm):
+    bound_field_class = CommentBoundField
+
+    class Meta:
+        model = Comment
+        fields = ["name", "email", "body"]
