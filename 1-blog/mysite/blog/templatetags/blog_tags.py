052a798666794eb9a6a011fc4190ded7baf58315
Florian BAUTRY <florian-f-externe.bautry@edf.fr>
2026-03-24 14:56:03 +0100

    feat: custom filter for Markdown syntax

--- 1-blog/mysite/blog/templatetags/blog_tags.py
+++ 1-blog/mysite/blog/templatetags/blog_tags.py
@@ -1,5 +1,7 @@
+import markdown
 from django import template
 from django.db.models import Count
+from django.utils.safestring import mark_safe
 
 from blog.models import Post
 
@@ -24,3 +26,10 @@ def get_most_commented_posts(count=5):
     return Post.published.annotate(total_comments=Count("comments")).order_by(
         "-total_comments"
     )[:count]
+
+
+# To prevent a name clash between the function name and the markdown module, we have named the function markdown_format
+# and we have named the filter markdown for use in templates, such as {{ variable|markdown }}.
+@register.filter(name="markdown")
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
