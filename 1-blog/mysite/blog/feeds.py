63bf2f8b7e039a09962bad663e3bd3cc9f70a21f
Florian BAUTRY <florian-f-externe.bautry@edf.fr>
2026-03-31 17:43:07 +0200

    feat: Creating feeds for blog posts

---
+++ 1-blog/mysite/blog/feeds.py
@@ -0,0 +1,28 @@
+import markdown
+from django.contrib.syndication.views import Feed
+from django.template.defaultfilters import truncatewords_html
+from django.urls import reverse_lazy
+
+from blog.models import Post
+
+
+class LatestPostsFeed(Feed):
+
+    title = "My blog"
+
+    # we use the lazy version of reverse because it allows to use a
+    # URL reversal before the project’s URL configuration is loaded.
+    link = reverse_lazy("blog:post_list")
+    description = "New posts of my blog."
+
+    def items(self):
+        return Post.published.all()[:5]
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return truncatewords_html(markdown.markdown(item.body), 30)
+
+    def item_pubdate(self, item):
+        return item.publish
